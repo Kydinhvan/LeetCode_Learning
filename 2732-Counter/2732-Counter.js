9d05242bd7af3c6d3db4f255984e2846c1a591c2
Kydinhvan <101254140+Kydinhvan@users.noreply.github.com>
2025-05-25 21:29:29 +0800

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,17 @@
+// Last updated: 5/25/2025, 9:29:28 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
