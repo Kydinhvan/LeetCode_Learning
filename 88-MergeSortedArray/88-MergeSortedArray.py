d69ed633f5aedd0c49b55b972e9843f63306c928
Ky Dinh Van <101254140+Kydinhvan@users.noreply.github.com>
2026-01-22 12:13:08 +0800

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,24 @@
+# Last updated: 1/22/2026, 12:13:07 PM
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # Sorted
+        # compare every index of n i and ii
+        # if mx < nx, i + 1, else i ++ and ii++
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        nums1.sort()
+
+
+
+
+
+            
+
+        
